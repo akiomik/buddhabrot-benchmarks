cbc12fbe01421bd173a361f6d8a0c899baabfa1c
Akiomi Kamakura <akiomik@gmail.com>
2025-11-29 01:19:38 +0900

    feat: add rust version

---
+++ rust/src/main.rs
@@ -0,0 +1,97 @@
+#![feature(random)]
+
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
+use std::random::random;
+
+fn main() -> Result<(), std::io::Error> {
+    let width = 1000;
+    let height = 1000;
+
+    let hist = buddhabrot(width, height, 1_000_000, 1_000);
+    let max = hist.iter().copied().max().unwrap_or(0);
+
+    write_pgm("buddhabrot.pgm", width, height, max, hist)
+}
+
+fn buddhabrot(width: usize, height: usize, samples: usize, max_iter: usize) -> Vec<u32> {
+    let mut hist = vec![0u32; width * height];
+
+    let xmin = -2.0;
+    let xmax = 1.0;
+    let ymin = -1.5;
+    let ymax = 1.5;
+
+    for _ in 0..samples {
+        let cr = rand_range(xmin, xmax);
+        let ci = rand_range(ymin, ymax);
+
+        let mut zr = 0.0;
+        let mut zi = 0.0;
+        let mut path = Vec::new();
+        path.reserve(max_iter);
+
+        let mut escaped = false;
+
+        for _ in 0..max_iter {
+            // z = z^2 + c
+            let zr2 = zr * zr - zi * zi + cr;
+            let zi2 = 2.0 * zr * zi + ci;
+            zr = zr2;
+            zi = zi2;
+
+            path.push((zr, zi));
+
+            if zr * zr + zi * zi > 4.0 {
+                escaped = true;
+                break;
+            }
+        }
+
+        if escaped {
+            for (xr, yi) in path {
+                if xmin <= xr && xr <= xmax && ymin <= yi && yi <= ymax {
+                    let px = ((xr - xmin) / (xmax - xmin) * width as f64) as isize;
+                    let py = ((yi - ymin) / (ymax - ymin) * height as f64) as isize;
+
+                    if px >= 0 && py >= 0 && (px as usize) < width && (py as usize) < height {
+                        hist[py as usize * width + px as usize] += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    return hist;
+}
+
+fn write_pgm(
+    path: impl AsRef<Path>,
+    width: usize,
+    height: usize,
+    max: u32,
+    data: Vec<u32>,
+) -> Result<(), std::io::Error> {
+    let file = File::create(path)?;
+    let mut out = BufWriter::new(file);
+    writeln!(out, "P2")?; // P2: Portable graymap (ASCII)
+    writeln!(out, "{} {}", width, height)?;
+    writeln!(out, "{}", max)?;
+
+    for v in data {
+        write!(out, "{} ", v)?;
+    }
+
+    Ok(())
+}
+
+// returns [0,1)
+fn rand() -> f64 {
+    let r: u32 = random(..);
+    (r as f64) / (u32::MAX as f64 + 1.0)
+}
+
+fn rand_range(a: f64, b: f64) -> f64 {
+    a + (b - a) * rand()
+}
